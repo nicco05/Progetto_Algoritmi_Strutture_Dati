4d53f26dd4ace69b05d2782c704d5174d1e3b30b
Niccolò <n.marchesi@studenti.unipi.it>
2026-08-13 09:37:29 +0200

    codice del blocco di caricamento n1

--- Progetto.cpp
+++ Progetto.cpp
@@ -1,21 +1,52 @@
-//caida: all-path 
-//vogliamo fare una struttura dati tale che dati 2 nodi del grafo quale è il peso minimo 
-//di un cammino(minimo massimo del peso di un arco nel cammino)
-// che collega dei 2 nodi e quali sono i cammini di peso minimo di questi 2 nodi e qaunto è lungo?
 #include<fstream>
 #include<iostream>
 #include<string>
+#include<sstream> //per trasformare la riga in un flusso di dati
+#include<algorithm> //per la funzione replace
 using namespace std;
 
-int main(int arg,char **argv){
+
+int main(int argc,char **argv){
+    // Verifica che sia stato passato un file come argomento
     if (argc <2){
-        cout << "specifica un file" <<endl;
+        cout << "Specifica un file" <<endl;
         return 0;
     }
-    cout << argv[0] << endl;
-    cout << argv[1] << end;
+    cout << "Programma: " << argv[0] << endl;
+    cout << "File: " << argv[1] << endl;
+
+    // Apertura del file specificato come argomento
     ifstream file(argv[1]);
-    string line;
-    getline(file, line);
-    cout << line << endl;
+
+    // Gestione di eventuali errori nell'apertura del file
+    if (!file.is_open()){
+        cout << "Errore nell'apertura del file" << endl;
+        return 1;
+    }
+
+    string line; // Variabile per memorizzare le righe lette dal file
+
+    // Lettura del file riga per riga (va avanti finche ci sono righe da leggere)
+    while (getline(file, line)) { 
+        stringstream ss(line); // trasforma la riga in un flusso di dati
+        string prima_parola, sequenza_as; // Variabili per memorizzare le prime 2 parole lette dalla riga
+
+        //l'operatore >> salta automaticamente gli spazi bianchi e legge le parole
+        if(ss >> prima_parola >> sequenza_as) { // Estrae le prime due parole dal flusso, if serve per verificare che ci siano almeno due parole nella riga
+            replace(sequenza_as.begin(), sequenza_as.end(), '|', ' '); // Sostituisce '|' con ' ' nella sequenza
+
+            //creiamo un nuovo flusso per leggere i numeri della sequenza_as modificata (con gli spazi al posto dei '|')
+            stringstream ss_numeri(sequenza_as);
+
+            int number; // Variabile per memorizzare i numeri letti dalla riga
+
+            while(ss_numeri >> number) { // Estrae i numeri dal flusso saltando gli spazi bianchi
+                cout << number << " "; //per ora lo stampo
+            }
+            cout << endl; //dopo ogni riga vado a capo
+        }
+    }
+    file.close(); // Chiusura del file dopo la lettura
+
+    return 0;
 }
